597454431da0e274453ad561c58622121ea33c7b
azilfozan <afozan@xiqinc.com>
2022-06-14 08:24:20 +0500

    copy files script added

---
+++ copy_files_arslan.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+from BI.settings import BASE_DIR, INSTALLED_APPS
+if __name__ == '__main__':
+    destination = os.path.join(BASE_DIR, 'BI', 'static')
+    for app in [_.split('.')[0] for _ in INSTALLED_APPS if not _.startswith('django')]:
+        source = os.path.join(BASE_DIR, app.split('.')[0], 'static')
+        if os.path.isdir(source):
+            os.system('cp -Rf {}/* {}/'.format(source, destination))
+            print('App "{}" static folder copied to {}'.format(app, destination))
+        else:
+            print ('App "{}" has no static folder. '.format(app))
