e04030262e8532ef68ee2b1c1ab4e52de1509636
azilfozan <afozan@xiqinc.com>
2022-08-16 07:34:33 +0500

    fix

--- BI/settings.py
+++ BI/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_DIR = os.path.dirname(__file__)
@@ -141,7 +142,7 @@ USE_TZ = True
 # https://warehouse.python.org/project/whitenoise/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'BI', 'media')
--- copy_files_arslan.py
+++ copy_files_arslan.py
@@ -3,7 +3,7 @@ import os
 from BI.settings import BASE_DIR, INSTALLED_APPS
 if __name__ == '__main__':
     destination = os.path.join(BASE_DIR, 'BI', 'static')
-    for app in [_.split('.')[0] for _ in INSTALLED_APPS if not _.startswith('django')]:
+    for app in [_.split('.')[0] for _ in INSTALLED_APPS if not _.startswith('django')] + ['django.contrib.staticfiles']:
         source = os.path.join(BASE_DIR, app.split('.')[0], 'static')
         if os.path.isdir(source):
             os.system('cp -Rf {}/* {}/'.format(source, destination))
